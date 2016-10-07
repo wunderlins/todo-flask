13074d9ea6484f2d1afe072835400557f64a0510
wus <swunderlin@gmail.com>
2016-10-07 16:28:52 +0200

    added ini

--- bin/uwsgi.sh
+++ bin/uwsgi.sh
@@ -2,6 +2,9 @@
 
 #uwsgi --plugin python,http --http :5000 --manage-script-name --mount /todo=bin/httpd:app
 
+echo "$(cat ./etc/todo.cfg | ./bin/ini2basharr.py)"
+exit
+
 uwsgi --http-socket 0.0.0.0:5000 \
       --plugin python \
       --wsgi-file ./bin/httpd \
