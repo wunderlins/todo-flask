61fae8455c85d795aeec3b063de27994e6c57b00
wunderlins <simon.wunderlin@usb.ch>
2016-10-11 07:31:27 +0200

    added context menu

--- bin/numlines.sh
+++ bin/numlines.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # print number of liens created in this porject
-for f in `find ./ -type f | grep -v ./.git/`; do 
+for f in `find ./ -type f | grep -v ./.git/ | grep -v './www/static/lib' | grep -v './build' | grep -v './dist' | grep -v './lib/site-packages' | grep -v '~$' | grep -v '.pyc$'`; do 
 	wc -l $f | sed -e 's/ .*//';
 done | paste -sd+ | bc
