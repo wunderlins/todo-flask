cb118a736b7b1db5384f551367d2ebaa3fd8ec7d
wus <swunderlin@gmail.com>
2016-10-13 16:47:02 +0200

    ignoring Makefiles and .gitignore

--- bin/numlines.sh
+++ bin/numlines.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # print number of liens created in this porject
-for f in `find ./ -type f | grep -v ./.git/ | grep -v './www/static/lib' | grep -v './build' | grep -v './dist' | grep -v './lib/site-packages' | grep -v '~$' | grep -v '.pyc$'`; do 
+for f in `find ./ -type f | grep -v ./.git/ | grep -v './www/static/lib' | grep -v './build' | grep -v './dist' | grep -v './lib/site-packages' | grep -v '~$' | grep -v '.pyc$' | grep -v Makefile | grep -v .gitignore`; do 
 	wc -l $f | sed -e 's/ .*//';
 done | paste -sd+ | bc
