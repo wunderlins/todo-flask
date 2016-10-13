e92f07e3ef078a7ed409195ef358d0b85dd1cca9
wus <swunderlin@gmail.com>
2016-10-13 16:58:06 +0200

    not counting test files

--- bin/numlines.sh
+++ bin/numlines.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # print number of liens created in this porject
-files=`find ./ -type f | grep -v ./.git/ | grep -v './www/static/lib' | grep -v './build' | grep -v './dist' | grep -v './lib/site-packages' | grep -v '~$' | grep -v '.pyc$' | grep -v Makefile | grep -v .gitignore | grep -v .bowerrc | grep -v ".md$" | grep -v ".db$" | grep -v ".spec$" | grep -v ".ini$"`
+files=`find ./ -type f | grep -v ./.git/ | grep -v './www/static/lib' | grep -v './build' | grep -v './dist' | grep -v './lib/site-packages' | grep -v '~$' | grep -v '.pyc$' | grep -v Makefile | grep -v .gitignore | grep -v .bowerrc | grep -v ".md$" | grep -v ".db$" | grep -v ".spec$" | grep -v ".ini$" | grep -v './test/'`
 echo "$files"
 for f in $files; do 
 	wc -l $f | sed -e 's/ .*//';
